d6e4a884ce28e48074ab778917a3b6869dd5bfaa
lek3 <106849854+lek3@users.noreply.github.com>
2025-09-03 14:25:25 -0300

    super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -21,11 +21,13 @@ int main() {
 
     // Entrada de dados - Carta 1
     printf("Carta 1:\n");
-    printf("Digite o Estado (letra de A a H):");
+    printf("Digite o Estado (letra de A a H): ");
     scanf(" %c", &estado1);
 
-    printf("Digite o Código da Carta (ex: A01): ");
-    scanf(" %s", codigo1);
+    printf("Digite o Código da Carta (ex: A01): \n");
+    scanf("%s", codigo1);
+    getchar(); // limpa o \n do buffera
+    
 
     printf("Digite o Nome da Cidade: ");
     scanf(" %[^\n]", nome_cidade1);
@@ -49,6 +51,7 @@ int main() {
 
     printf("Digite o Código da Carta (ex: B03): ");
     scanf("%s", codigo2);
+    getchar(); // limpa o \n do buffer
 
     printf("Digite o Nome da Cidade: ");
     scanf(" %[^\n]", nome_cidade2);
